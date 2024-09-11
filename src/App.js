40eec5c59d25bc3cdb88c1c752907f20b7de0ded
Jawad Hussain <jawadduet@gmail.com>
2024-09-11 12:11:23 +0500

    Render multiple elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
 
 function App() {
   return (
-   <h1>Hello World!</h1>
+<React.Fragment> 
+  <h1>Hello World!</h1>
+   <h2>From Jawad Hussain</h2>
+   </React.Fragment>
+  
         
   );
 }
