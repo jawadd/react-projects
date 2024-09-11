cf090e4e96b501bb874ef1ca612e2f2573943218
Jawad Hussain <jawadduet@gmail.com>
2024-09-11 23:05:33 +0500

    JS Expressions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import React  from 'react';
 
 function App() {
+  const stat= "Learn React with Jawad";
   return (
+    
 <React.Fragment> 
   <h1>Hello World!</h1>
    <h2>From Jawad Hussain</h2>
+   <h3>{stat}</h3>
    </React.Fragment>
   
         
