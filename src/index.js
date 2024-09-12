f381079f69948e89e725f993eb202ced2e82654a
Jawad Hussain <jawadduet@gmail.com>
2024-09-12 12:29:40 +0500

    React Components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home'
+import About from './About'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    <About />
   </React.StrictMode>
 );
 
