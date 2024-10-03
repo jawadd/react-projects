ed7c5cfcffe0d0fad849e78fdaa2d7562ff5735d
Jawad Hussain <jawadduet@gmail.com>
2024-10-03 10:09:17 +0500

    Nested routes

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Link,NavLink,useParams,useLocation } from "react-router-dom";
+import { NavLink,useParams,useLocation } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 
 const Login = () => {
   const {fname,lname}= useParams();
-  const location = useLocation();
+  
   
   return (
     <div>
@@ -18,7 +19,17 @@ const Login = () => {
         </nav>
 
       <h1> Hello {fname} {lname} </h1>
-      <h1> You are currently on {location.pathname} location  </h1>
+    
+      <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
     </div>
   )
 }
